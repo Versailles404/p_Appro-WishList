097d234047914d6ee38474cd00d5f7ff55f56feb
ETML-VGZ-Mary <143503804+ETML-VGZ-Mary@users.noreply.github.com>
2023-12-22 14:37:01 +0100

    22.12.2023

--- P_Appro-WishList/02-controller/metDataBase.php
+++ P_Appro-WishList/02-controller/metDataBase.php
@@ -2,7 +2,7 @@
 
 class Database{
 
-    function conDB()
+    function connectDB() /*can put a bool*/
     {
         $connector = new PDO("localhost:8080", "root", "root");
         
--- P_Appro-WishList/02-controller/scrapper.py
+++ P_Appro-WishList/02-controller/scrapper.py
@@ -5,23 +5,27 @@ import requests
 # add lib
 from bs4 import BeautifulSoup
 
-def scrap()
-    {
-        return fetchedDatas
-    }
+with open("https://www.parfumdo.com/87-parfum-femme") as pageToParse:
+    fetchedDatas = BeautifulSoup(pageToParse)
+    fetchedDatas = BeautifulSoup("<span class=\"product-category\">MISS DIOR</span>", 
+                                 "<span class=\"product-brand\"> Dior </span>", 
+                                 "<span class=\"product-name\"> Eau de Parfum </span>",
+                                 "<img src=<\"https://www.parfumdo.com/53987-home_default/miss-dior-eau-de-parfum.jpg\" alt=\"MISS DIOR Eau de Parfum\" loading=\"lazy\">",
+                                 "<a href=\"https://www.parfumdo.com/417545-miss-dior-eau-de-parfum.html\" title=\"MISS DIOR Eau de Parfum\" class=\"\"> <span class=\"product_img_link\"> <img src=\"https://www.parfumdo.com/53987-home_default/miss-dior-eau-de-parfum.jpg\" alt=\"MISS DIOR Eau de Parfum<\" loading=\"lazy\"> </span> </a>")
 
-# Galaxus struct. 1page = 1prod
+# Parfumdo struct. 1page = 1prod
 #
 # Nom
-#   <span class="sc-1l1ysqm-0 bDSRlN">AirPods Pro (2nd Gen.) MagSafe USB-C</span>
+#   <span class="product-category">MISS DIOR</span>
 # Marque
-#   <strong>Apple</strong>
+#   <span class="product-brand"> Dior </span>
 # Carac.
-#   <span class="sc-12r9jwk-1 fjTjcd">ANC, 6&nbsp;h, Sans fil</span>
+#   <span class="product-name"> Eau de Parfum </span>
 # Prix
-#   <span aria-label="225 CHF">225.–</span>
+#   <span class="hidden"> 56.06 </span>
 # IMG
-#   <img class="sc-1ienw2c-1 jXozAl" src="/im/productimages/3/5/0/3/1/7/6/3/7/2/2/4/8/1/7/4/0/1/5/
-#   b2efad0f-fc3a-4650-9d9b-3924bdf99e2b.jpg?impolicy=ProductTileImage&amp;resizeWidth=358&amp;resize
-#   Height=358&amp;cropWidth=358&amp;cropHeight=358&amp;resizeType=downsize&amp;quality=high" decoding=
-#  "auto" alt="Image du produit" loading="eager">
+#   <img src="https://www.parfumdo.com/53987-home_default/miss-dior-eau-de-parfum.jpg" alt="MISS DIOR Eau de Parfum" loading="lazy">
+# Lien
+#   <a href="https://www.parfumdo.com/417545-miss-dior-eau-de-parfum.html" title="MISS DIOR Eau de Parfum" class=""> 
+#   <span class="product_img_link"> <img src="https://www.parfumdo.com/53987-home_default/miss-dior-eau-de-parfum.jpg" 
+#   alt="MISS DIOR Eau de Parfum" loading="lazy"> </span> </a>
